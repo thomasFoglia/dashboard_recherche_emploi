afeb1b88210875450cd3953428a09fc629b47049
thomasFoglia <thomas.foglia38@gmail.com>
2017-12-26 15:42:17 +0100

    css

--- views/liste.php
+++ views/liste.php
@@ -16,7 +16,7 @@
               <i class="fa fa-fw fa-list"></i>
             </div>
             <div class="mr-5"><?= $total ?> candidatures au total
-              <br><span style='font-size:13px;'><i><?= $nb_en_attente ?> en cours</i></span>
+              <br><span style='font-size:13px;'><b><i><?= $nb_en_attente ?> en cours</i></b></span>
               <span style='font-size:13px;'><i>et <?= $nb_refus ?> refus</i></span>
             </div>
           </div>
@@ -28,98 +28,99 @@
             <div class="card-body-icon">
               <i class="fa fa-fw fa-comments"></i>
             </div>
-            <div class="mr-5"><?= $nb_candidatures_last_days ?> nouvelles candidatures<br>ces <?= $nb_days_created_last_days ?> derniers jours</div>
+            <div class="mr-5"><?= $nb_candidatures_last_days ?> nouvelles candidatures<br>
+              <span style='font-size:13px;'>ces <?= $nb_days_created_last_days ?> derniers jours</div></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-4 col-sm-6 mb-3">
+          <?php
+          $class = "bg-danger";
+          if ($total_a_relancer == 0) {
+            $class = "bg-success";
+          }
+          ?>
+          <div class="card text-white <?= $class ?> o-hidden h-100">
+            <div class="card-body">
+              <div class="card-body-icon">
+                <i class="fa fa-fw fa-exclamation-circle"></i>
+              </div>
+              <div class="mr-5"><?= $total_a_relancer ?> entreprises <br><span style='font-size:13px;'>à relancer</span></div>
+            </div>
           </div>
         </div>
       </div>
-
-      <div class="col-xl-4 col-sm-6 mb-3">
-        <?php
-        $class = "bg-danger";
-        if ($total_a_relancer == 0) {
-          $class = "bg-success";
-        }
-        ?>
-        <div class="card text-white <?= $class ?> o-hidden h-100">
+      <!-- Example DataTables Card-->
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fa fa-table"></i> Liste des demandes</div>
           <div class="card-body">
-            <div class="card-body-icon">
-              <i class="fa fa-fw fa-exclamation-circle"></i>
-            </div>
-            <div class="mr-5"><?= $total_a_relancer ?> entreprises à relancer</div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Example DataTables Card-->
-    <div class="card mb-3">
-      <div class="card-header">
-        <i class="fa fa-table"></i> Liste des demandes</div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" style='font-size:11px;'>
-              <thead>
-                <tr>
-                  <th style='display:none;'>ID</th>
-                  <th>Date demande</th>
-                  <th>Entreprise</th>
-                  <th>Adresse</th>
-                  <th>Mail</th>
-                  <th>Téléphone</th>
-                  <th>Lien annonce</th>
-                  <th>Commentaire</th>
-                  <th>Rappelé?</th>
-                  <th>Réponse</th>
-                </tr>
-              </thead>
-              <tbody>
-                <?php foreach ($all_candidatures as $cand) { ?>
+            <div class="table-responsive">
+              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" style='font-size:11px;'>
+                <thead>
                   <tr>
-                    <td class='id_to_update' style='display:none;'><?= $cand["id"] ?></td>
-                    <td><?= date("d/m/Y", strtotime($cand["dateDemande"])); ?></td>
-                    <td><?= $cand["entreprise"] ?></td>
-                    <td><a target=_blank href="https://www.google.fr/maps/place/<?= $cand["adresse"] ?>"><?= $cand["adresse"] ?></td>
-                      <td><?= $cand["mail"] ?></td>
-                      <td><?= $cand["telephone"] ?></td>
+                    <th style='display:none;'>ID</th>
+                    <th>Date demande</th>
+                    <th>Entreprise</th>
+                    <th>Adresse</th>
+                    <th>Mail</th>
+                    <th>Téléphone</th>
+                    <th>Lien annonce</th>
+                    <th>Commentaire</th>
+                    <th>Rappelé?</th>
+                    <th>Réponse</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php foreach ($all_candidatures as $cand) { ?>
+                    <tr>
+                      <td class='id_to_update' style='display:none;'><?= $cand["id"] ?></td>
+                      <td><?= date("d/m/Y", strtotime($cand["dateDemande"])); ?></td>
+                      <td><?= $cand["entreprise"] ?></td>
+                      <td><a target=_blank href="https://www.google.fr/maps/place/<?= $cand["adresse"] ?>"><?= $cand["adresse"] ?></td>
+                        <td><?= $cand["mail"] ?></td>
+                        <td><?= $cand["telephone"] ?></td>
 
-                      <?php
-                      echo "<td>";
-                      $array_liens = explode(" ", $cand["lien_annonce"]);
-                      foreach ($array_liens as $lien) {
-                        if ($lien != "") {
-                          echo "<a target=_blank href=$lien>$lien<br>";
+                        <?php
+                        echo "<td>";
+                        $array_liens = explode(" ", $cand["lien_annonce"]);
+                        foreach ($array_liens as $lien) {
+                          if ($lien != "") {
+                            echo "<a target=_blank href=$lien>$lien<br>";
+                          }
                         }
-                      }
-                      echo "</td>";
-                      ?>
+                        echo "</td>";
+                        ?>
 
-                      <td><?= $cand["commentaire"] ?></td>
-                      <?php
-                      if($cand["reponse"] == "non") {
-                        $dateRappel = "Non";
-                      } else if($cand["dateRappel"] == null) {
-                        $dateRappel = "<button type='button' class='btn_rappel btn btn-outline-secondary' style='width:100%'>Oui</button>";
-                      } else {
-                        $dateRappel = date("d/m/Y", strtotime($cand["dateRappel"]));
-                      }
-                      ?>
-                      <td><?= $dateRappel ?></td>
-                      <td>
-                        <?php if($cand["reponse"] == "non") {
-                          echo "Non";
-                        } else { ?>
-                          <select class="btn_reponse form-control">
-                            <option></option>
-                            <option value='non'>Non</option>
-                          </select>
-                        <?php } ?>
-                      </td>
-                    </tr>
-                  <?php } ?>
-                </tbody>
-              </table>
+                        <td><?= $cand["commentaire"] ?></td>
+                        <?php
+                        if($cand["reponse"] == "non") {
+                          $dateRappel = "Non";
+                        } else if($cand["dateRappel"] == null) {
+                          $dateRappel = "<button type='button' class='btn_rappel btn btn-outline-secondary' style='width:100%'>Oui</button>";
+                        } else {
+                          $dateRappel = date("d/m/Y", strtotime($cand["dateRappel"]));
+                        }
+                        ?>
+                        <td><?= $dateRappel ?></td>
+                        <td>
+                          <?php if($cand["reponse"] == "non") {
+                            echo "Non";
+                          } else { ?>
+                            <select class="btn_reponse form-control">
+                              <option></option>
+                              <option value='non'>Non</option>
+                            </select>
+                          <?php } ?>
+                        </td>
+                      </tr>
+                    <?php } ?>
+                  </tbody>
+                </table>
+              </div>
             </div>
+            <!-- <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div> -->
           </div>
-          <!-- <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div> -->
         </div>
       </div>
-    </div>
